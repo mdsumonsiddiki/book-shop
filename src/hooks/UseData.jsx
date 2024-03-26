b848934e74063dd2207dc0033ee86d15aedf1c64
Md Sumon Siddiki <mdsuman.cit.tool@gmail.com>
2024-03-26 18:01:43 +0600

    books details page done

--- src/hooks/UseData.jsx
+++ src/hooks/UseData.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const UseData = () => {
     const [books, setBooks] = useState([])
     useEffect(()=>{
-        fetch('/public/books.json')
+        fetch('books.json')
         .then(res => res.json())
         .then(data => setBooks(data));
     },[])
